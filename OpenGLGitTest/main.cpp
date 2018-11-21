9382a28cbbb8e29d32542f5a3096e04dfac620e8
Aaron Dorrance <adorranc@nd.edu>
2018-11-21 18:40:07 -0500

    Test Change to the Project

--- OpenGLGitTest/main.cpp
+++ OpenGLGitTest/main.cpp
@@ -53,7 +53,7 @@ int main()
 		"out vec4 FragColor;\n"
 		"void main()\n"
 		"{\n"
-		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"	FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
 		"}\n\0";
 
 	// Compile the Vertex Shader 
